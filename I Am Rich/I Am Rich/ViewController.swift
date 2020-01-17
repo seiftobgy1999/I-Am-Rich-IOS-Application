0d997d6fde8ba9e5f4b520cc922bc1d4e6f0d000
Seif El Tobgy <seifeltobgy@Seifs-MacBook-Air.local>
2020-01-17 00:34:35 -0500

    Added files for application

---
+++ I Am Rich/I Am Rich/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  I Am Rich
+//
+//  Created by Seif El Tobgy on 2020-01-16.
+//  Copyright © 2020 Seif El Tobgy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
